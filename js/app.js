f116fd54957e6cf86ccc929268cec66701c20389
G-nizam-A <nizamcs10@gmail.com>
2023-12-25 18:59:47 +0530

    final

--- js/app.js
+++ js/app.js
@@ -8,4 +8,16 @@ document.querySelectorAll('.slider').forEach((n, i) => {
 		slidesOffsetBefore: -125
 	})
 })
-bindSwipers(slider1, slider2, slider3, slider4)
+bindSwipers(slider1, slider2, slider3, slider4);
+
+
+
+document.addEventListener('mousemove', e => {
+	Object.assign(document.documentElement, {
+		style: `
+		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+		--move-y: ${(e.clientY - window.innerHeight / 2) * .01}deg;
+		`
+	})
+})
+
